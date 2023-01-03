6ca890131a45a7c641a91c50c3d47ebec30c7c40
maciejors <maciej.orslowski@gmail.com>
2023-01-03 18:27:23 +0100

    add button css classes and a Card component

--- montrealestate.client/src/routes/+page.svelte
+++ montrealestate.client/src/routes/+page.svelte
@@ -1,5 +1,19 @@
+<script>
+  import Card from "../shared/Card.svelte";
+</script>
+
+<style>
+  /* background img: https://picsum.photos/id/223/200/300 */
+</style>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div class="mx-10">
   <p>hello</p>
+  <Card>
+    <p>this is a card</p>
+    <a class="btn" href="">a plain button</a>
+    <a class="btn btn-primary" href="">primary button</a>
+    <a class="btn btn-secondary" href="">secondary button</a>
+  </Card>
 </div>
---
+++ montrealestate.client/src/shared/Card.svelte
@@ -0,0 +1,11 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card {
+    padding: 5px;
+    border-radius: 5px;
+    box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.4);
+  }
+</style>
